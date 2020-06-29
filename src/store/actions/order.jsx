4773504fd8b8435fc46492316496d767fd28097b
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-06-29 20:02:02 +0700

    Change class-based to Hooks

--- src/store/actions/order.jsx
+++ src/store/actions/order.jsx
@@ -1,4 +1,5 @@
 import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
 
 export const purchaseBurgerSuccess = (id, orderData) => {
 	return {
@@ -22,10 +23,16 @@ export const purchaseBurgerStart = () => {
 };
 
 export const purchaseBurger = (orderData, token) => {
-	return {
-		type: actionTypes.PURCHASE_BURGER,
-		orderData: orderData,
-		token: token,
+	return (dispatch) => {
+		dispatch(purchaseBurgerStart());
+		axios
+			.post("/orders.json?auth=" + token, orderData)
+			.then((response) => {
+				dispatch(purchaseBurgerSuccess(response.data.name, orderData));
+			})
+			.catch((error) => {
+				dispatch(purchaseBurgerFail(error));
+			});
 	};
 };
 
@@ -56,9 +63,24 @@ export const fetchOrdersStart = () => {
 };
 
 export const fetchOrders = (token, userId) => {
-	return {
-		type: actionTypes.FETCH_ORDERS,
-		token: token,
-		userId: userId,
+	return (dispatch) => {
+		dispatch(fetchOrdersStart());
+		const queryParams =
+			"?auth=" + token + '&orderBy="userId"&equalTo="' + userId + '"';
+		axios
+			.get("/orders.json" + queryParams)
+			.then((res) => {
+				const fetchedOrders = [];
+				for (let key in res.data) {
+					fetchedOrders.push({
+						...res.data[key],
+						id: key,
+					});
+				}
+				dispatch(fetchOrdersSuccess(fetchedOrders));
+			})
+			.catch((err) => {
+				dispatch(fetchOrdersFail(err));
+			});
 	};
 };
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
 
 import burgerBuilderReducer from "./reducers/burgerBuilder";
 import orderReducer from "./reducers/order";
 import authReducer from "./reducers/auth";
-import { watchAuth, watchBurgerBuilder, watchOrder } from "./sagas";
 
 const composeEnhancers =
 	process.env.NODE_ENV === "development"
@@ -18,15 +16,9 @@ const rootReducer = combineReducers({
 	auth: authReducer,
 });
 
-const sagaMiddleware = createSagaMiddleware();
-
 const store = createStore(
 	rootReducer,
-	composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
+	composeEnhancers(applyMiddleware(thunk))
 );
-
-sagaMiddleware.run(watchAuth);
-sagaMiddleware.run(watchBurgerBuilder);
-sagaMiddleware.run(watchOrder);
 
 export default store;
